cd6b4d8a1309a609bee10eaefe0c6407c97b8d8d
mitica-criste-vinci <mitica.criste@student.vinci.be>
2026-01-16 16:11:42 +0100

    refactor : extract EmailValidator instanciation

--- src/EmailValidator.test.ts
+++ src/EmailValidator.test.ts
@@ -1,16 +1,18 @@
 import { EmailValidator } from "./EmailValidator";
 
 describe("EmailValidator", () => {
-  it("should return false when given empty string", () => {
     let validator = new EmailValidator();
+
+  it("should return false when given empty string", () => {
     const actual = validator.isValid("");
+
     expect(actual).toBe(false);
   });
 
 
   it("should return true when given string with one @", () => {
-  let validator = new EmailValidator();
   const actual = validator.isValid("test@mail.com");
+  
   expect(actual).toBe(true);
 });
 
